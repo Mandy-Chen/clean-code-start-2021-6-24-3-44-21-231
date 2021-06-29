f4339bd9a337f69cf50e2bd1d9c375662d7280ac
CHENMA18 <chenma18@oocl.com>
2021-06-29 15:08:20 +0800

    Extract into class

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -2,23 +2,18 @@ package com.tw.academy.basic.$5_data_clumps;
 
 public class Order {
 
+    private final User buyer;
     private int orderNumber;
 
-    private String buyerName;
-    private String buyerPhoneNumber;
-    private String buyerAddress;
 
-    public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
+    public Order(int orderNumber, User buyer) {
         this.orderNumber = orderNumber;
-
-        this.buyerName = buyerName;
-        this.buyerPhoneNumber = buyerPhoneNumber;
-        this.buyerAddress = buyerAddress;
+        this.buyer = buyer;
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", this.buyer.getBuyerName(), this.buyer.getBuyerPhoneNumber(), this.buyer.getBuyerAddress());
     }
 }
 
